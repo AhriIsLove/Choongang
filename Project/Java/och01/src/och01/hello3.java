83401cc2e6a7d3464638df9d921ba2464e1326a9
AhriIsLove <60705922+AhriIsLove@users.noreply.github.com>
2025-06-11 23:48:26 +0900

    Java First
    Java
    - och01 : HelloWorld
    - och02 : 데이터타입, 연산자

---
+++ Project/Java/och01/src/och01/hello3.java
@@ -0,0 +1,11 @@
+package och01;
+
+public class hello3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("class명 소문자 안 된다는데...");
+		System.out.println("오류 안남, 하지만 쓰지마라");
+	}
+
+}
